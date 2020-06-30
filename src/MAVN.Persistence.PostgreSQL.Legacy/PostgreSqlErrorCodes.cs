dfbd4fa597d9882016f1abcf1d5d4c2e751d03fe
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-06-30 11:41:38 +0300

    Add postgre error codes

---
+++ src/MAVN.Persistence.PostgreSQL.Legacy/PostgreSqlErrorCodes.cs
@@ -0,0 +1,13 @@
+﻿namespace MAVN.Persistence.PostgreSQL.Legacy
+{    
+    /// <summary>
+    /// Postgre SQL  error codes
+    /// </summary>
+    public static class PostgreSqlErrorCodes
+    {
+        /// <summary>
+        /// Cannot insert duplicate key row 
+        /// </summary>
+        public const int UniqueViolation = 23505;
+    }
+}
